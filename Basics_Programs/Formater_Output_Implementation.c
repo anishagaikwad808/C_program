dc64a567de258d50065f75f25e374378b59cc86c
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-09-27 21:00:48 +0530

    Add files via upload

---
+++ Basics_Programs/Formater_Output_Implementation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+  {
+      printf("\n Student information database :\n");
+   
+        printf("\n %4d %-7s %3d %3d %3d %.2f",255,"Amit",9,81,150,50.00);
+        printf("\n %4d %-7s %3d %3d %3d %.2f",02,"Sujata",70,80,200,66.667);
+        printf("\n %4d %-7s %3d %3d %3d %.2f",150,"Ajay",82,18,170,56.6677);
+    return 0;
+  }
