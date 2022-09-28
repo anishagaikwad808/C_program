b36b088a4a092ae19a3dd67f7db21e4a3b46328f
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-09-28 20:10:48 +0530

    Add files via upload

---
+++ Array_Programs/Array_without_Initilization_And_Display_All_Elements.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  int Arr[]={2,43,5,67};
+    printf("value of 1 element =%d\n",Arr[0]);
+    printf("value of 2 element=%d\n",Arr[1]);
+    printf("value of 3 element=%d\n",Arr[2]);
+    printf("value of 4 element=%d\n",Arr[3]);
+     return 0;
+} 
