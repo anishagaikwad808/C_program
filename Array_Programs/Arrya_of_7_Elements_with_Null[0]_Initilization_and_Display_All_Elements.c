27f08b71dd6862cd9f6ca059f47343bc32d2b8a5
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-09-28 20:27:27 +0530

    Add files via upload

---
+++ Array_Programs/Arrya_of_7_Elements_with_Null[0]_Initilization_and_Display_All_Elements.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int Arr[]={11,12,34,43,3,4,5};
+    printf("value of 1 element =%d\n",Arr[0]);
+    printf("value of 2 element=%d\n",Arr[1]);
+    printf("value of 3 element=%d\n",Arr[2]);
+    printf("value of 2 element=%d\n",Arr[3]);
+    printf("value of 2 element=%d\n",Arr[4]);
+    printf("value of 2 element=%d\n",Arr[5]);
+    printf("value of 2 element=%d\n",Arr[6]);      
+    return 0;
+} 
