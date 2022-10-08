20b35e10f3b512d2d3556944f373d914169a40f1
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-08 07:20:32 +0530

    Add files via upload

---
+++ Pattern_Printing/T_pattern_printing.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int C=0,R=0,rCnt;
+    printf("Enter a Pattern by Odd Number=\n");
+    scanf("%d",&rCnt);
+    
+    for(R=1;R<=rCnt;R++)
+    {
+     for(C=1;C<=rCnt;C++)
+     {
+      if(R==1||C==rCnt/2+1)
+      printf(" * ");
+      else
+     printf("   ");
+     }
+      printf("\n");
+    }
+    return 0;
+}
