c285b6a2009b7bbb5e88867a162b054dec34b346
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-08 07:21:16 +0530

    Add files via upload

---
+++ Pattern_Printing/C_pattern_printing.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int C=0,R=0,rCnt;
+    printf("Enter a Pattern=\n");
+    scanf("%d",&rCnt);
+    
+    for(R=1;R<=rCnt;R++)
+    {
+     for(C=1;C<=rCnt;C++)
+     {
+      if(R==1||R==rCnt||C==1)
+      printf(" * ");
+      else
+     printf("   ");
+     }
+      printf("\n");
+    }
+    return 0;
+}
