efb369b077c9ae96ec3fa557a0572eae42bafb5b
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-14 22:30:46 +0530

    Add files via upload

---
+++ ATM_Program_in_C.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+ float x,y;
+ char ch;
+ printf("Enter initial amount=");
+ scanf("%f",&x);
+  printf("Enter\nc for credit\nd for debit\nb for balance\n");
+  scanf("\n%c",&ch);
+  
+ switch(ch)
+  {
+  case'c':
+  printf("Enter creadit amount=");
+  scanf("%f",&y);
+  x=x+y;
+  printf("New amount=%f",x);
+  break;
+  case'd':
+  printf("Enter debit amount=");
+  scanf("%f",&y);
+  if(x>=y)
+ {
+  x=x-y;
+  printf("New amount=%f",x);
+  }
+  else
+  {
+  printf("insufficient amount in your account");
+ }
+  break;
+ case'b':
+printf("Amount in your account=%f",x);
+ break;
+  default:
+  printf("chosse option of Operation");
+
+}
+
+}
