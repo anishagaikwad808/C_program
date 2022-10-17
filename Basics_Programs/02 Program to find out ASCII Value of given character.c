6c8785db3a5560a7e6d794d798664020f02d3d5a
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-18 00:07:05 +0530

    Add files via upload

---
+++ Basics_Programs/02 Program to find out ASCII Value of given character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int Val=0;
+   
+  printf("\n ENTER THE ASCII NUMBER :\n");
+  scanf("%d",&Val);
+  
+     printf("\n The ASCII Value of %c is %d.",Val,Val);
+   
+ 
+  getch();
+  return 0;
+}
