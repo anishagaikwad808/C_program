c42016bf6fcab75bbb2e37149fb1b1748dfc103d
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-18 08:14:19 +0530

    Update and rename Array_with_Initilization_and_Display_All_elements.c to Array with Initilization and Display All Elements.c

--- Array_Programs/Array with Initilization and Display All Elements.c
+++ Array_Programs/Array with Initilization and Display All Elements.c
@@ -3,9 +3,12 @@
 int main()
 {
   int Arr[4]={2,4,6,8};
+
     printf("value of 1 element =%d\n",Arr[0]);
     printf("value of 2 element=%d\n",Arr[1]);
     printf("value of 3 element=%d\n",Arr[2]);
     printf("value of 4 element=%d\n",Arr[3]);
-     return 0;
+     
+ getch();
+ return 0;
 } 
