644415b1d50489ff3192456b7e678acfd1078dd4
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-18 07:16:41 +0530

    Add files via upload

---
+++ Digits_Programs/01 Program Calculate Number of digits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,dCnt=0,Temp=0;
+    
+    printf("\n Enter positive Integer Number To Calculate it's digit Number=");
+    scanf("%d",&No);
+    Temp=No;
+    
+     while(Temp>0)
+     {
+       dCnt++;
+       Temp=Temp/10;
+     }
+     printf("\n No of digits in given Number %d is=%d.",No,dCnt);
+    
+     getch();
+     return 0;
+}
