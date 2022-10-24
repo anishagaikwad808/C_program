96f4033eb6ec37be8a2d1009eb880a44eb684fa8
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-24 22:16:33 +0530

    Update and rename Subtraction of two numbers.c to Subtraction of Two Numbers.c

--- Basics_Programs/Subtraction of Two Numbers.c
+++ Basics_Programs/Subtraction of Two Numbers.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-   int Number1,Number2,Sum;
+   int Number1,Number2,Sub;
    
   printf("Enter a two Numbers:");
   scanf ("%d%d",&Number1,&Number2);
   
-  Sum=Number1-Number2;
-  printf("Subtraction of %d+%d=%d",Number1,Number2,Sum);
+  Sub=Number1-Number2;
+  printf("Subtraction of %d+%d=%d",Number1,Number2,Sub);
   
   getch();
   return 0;
