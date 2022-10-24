e71b182a497af909cc2012d3d6026f529a1b358c
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-10-24 22:10:30 +0530

    Add files via upload

---
+++ Basics_Programs/Subtraction of two numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int Number1,Number2,Sum;
+   
+  printf("Enter a two Numbers:");
+  scanf ("%d%d",&Number1,&Number2);
+  
+  Sum=Number1-Number2;
+  printf("Subtraction of %d+%d=%d",Number1,Number2,Sum);
+  
+  getch();
+  return 0;
+}
