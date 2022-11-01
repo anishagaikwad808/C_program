54a5beda1e7cb541c91b61c3be54cd39175764b4
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-11-01 09:02:55 +0530

    Update and rename lowercase.c to Program to Convert Uppercase into Lowercase.c

--- Strings_Programs/Program to Convert Uppercase into Lowercase.c
+++ Strings_Programs/Program to Convert Uppercase into Lowercase.c
@@ -5,19 +5,20 @@ int main()
     char csrc[50]={'\0'};
     int i=0;
     
-    puts("\n enter a string=");
+    puts("\n Enter a string=");
     gets(csrc);
     
     while(csrc[i]!='\0')
     {
       if(csrc[i]>='A'&&csrc[i]<='Z')
-    {
-      csrc[i]=csrc[i]+32;
-    }
-    
+     {
+       csrc[i]=csrc[i]+32;
+     }
      i++;
     }
-    printf("lower case string is=%s",csrc);
+
+    printf("Lowercase string is=%s",csrc);
     
+    getch();
     return 0; 
  }   
