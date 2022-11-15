d86cde1e09cad7b575bafacdd14a9f80f24a33e8
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2022-11-15 09:29:00 +0530

    Add files via upload

---
+++ Strings_Programs/program to count Vowel in string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int i = 0, count = 0;
+  char s[40];
+
+  printf("Input a string\n");
+  gets(s);
+
+  while (s[i] != '\0')
+   {
+    if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'i' || s[i] == 'I' || s[i] =='o' || s[i]=='O' || s[i] == 'u' || s[i] == 'U')
+      count++;
+    i++;
+  }
+
+  printf("Number of vowels in the string: %d", count);
+
+  return 0;
+}
