9bce1297e180b81d57548d1e984d1fdb9cbc2121
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2023-04-03 23:47:42 +0530

    Add files via upload

---
+++ Function/Factorial Using Function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int factorial(int n)
+ {
+   int fact = 1, i;
+  
+   for (i = 1; i <= n; i++)
+       fact =i*fact;
+  
+   return fact;
+ }
+
+int main() 
+ {
+   int n;
+  
+   printf("Enter a number: ");   
+   scanf("%d",&n);
+ 
+   
+   printf("Factorial of %d is %d", n,factorial(n));
+  
+   return 0;
+}
